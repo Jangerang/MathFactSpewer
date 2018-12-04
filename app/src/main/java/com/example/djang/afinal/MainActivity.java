5996aa4d9ab0ecd1a249cbbe2e1425f96be7f5e7
Daniel Jang <djang21@illinois.edu>
2018-12-03 23:10:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/djang/afinal/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.djang.afinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button randomButton = findViewById(R.id.buttonRandom);
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyApp", "Random Button was pressed.");
+            }
+        });
+
+        Button saveButton = findViewById(R.id.buttonSave);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyApp", "Save Button was pressed.");
+            }
+        });
+    }
+
+}
