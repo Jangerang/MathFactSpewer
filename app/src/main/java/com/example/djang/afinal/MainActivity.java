628b6a432d1f291f1bd5e4c0b483007a0a73d046
JKTKops <zerglingk9012@gmail.com>
2018-12-04 21:06:18 -0600

    Setup for backend.

--- app/src/main/java/com/example/djang/afinal/MainActivity.java
+++ app/src/main/java/com/example/djang/afinal/MainActivity.java
@@ -5,36 +5,77 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "FinalProject:Main";
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
+
+    /** Possible request types */
+    private enum TYPES {TRIVIA, DATE, YEAR, MATH};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        // Set up a queue for our Volley requests
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Load the main layout for our activity
         setContentView(R.layout.activity_main);
 
         Button randomButton = findViewById(R.id.buttonRandom);
         randomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("MyApp", "Random Button was pressed.");
+                Log.i(TAG, "Random Button was pressed.");
             }
         });
 
-        Button saveButton = findViewById(R.id.buttonGenerate);
+        Button saveButton = findViewById(R.id.buttonSave);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("MyApp", "Generate Button was pressed.");
+                Log.i(TAG, "Generate Button was pressed.");
+                startApiCall();
             }
         });
-
-        Spinner dropdown = findViewById(R.id);
-
-
-
     }
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://numbersapi.com/random/math?json",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            apiCallDone(response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    ((TextView) findViewById(R.id.textDisplay)).setText(getString(R.string.ApiError));
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 }
