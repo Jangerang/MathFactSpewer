91a7aa20446a5a76e667a4b289ea9e6bf8e2a0bf
Daniel Jang <djang21@illinois.edu>
2018-12-04 20:00:33 -0600

    12/4 8:00pm

--- app/src/main/java/com/example/djang/afinal/MainActivity.java
+++ app/src/main/java/com/example/djang/afinal/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -31,10 +32,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Spinner dropdown = findViewById(R.id);
+        Spinner dropdown = findViewById(R.id.dropdownOptions);
 
-
-
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(myAdapter);
     }
 
 }
